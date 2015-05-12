2ff637058738a09c337662ed1e6ba7d3bde4647f
Genesis Palaganas <itetsky123@gmail.com>
2015-05-12 22:15:23 +0800

    Added Empty Main class and added compile and run bash scripts for easier compiling and running

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.transition.*;
+import org.newdawn.slick.state.GameState;
+import org.newdawn.slick.Input;
+
+//Main Class of the Game
+public class Main{
+    
+
+    public static void main(){
+        
+
+
+    }
+
+}
