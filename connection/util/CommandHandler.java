c778b3ccf4a7ec49a1910c48d827dfb64569f56d
Genesis Palaganas <itetsky123@gmail.com>
2015-05-12 22:06:49 +0800

    Added some files in gitignore

--- connection/util/CommandHandler.java
+++ connection/util/CommandHandler.java
@@ -1,9 +1,19 @@
 package connection.util;
 
+//Handles Extraction of commands and parameters given a message sent by server or client
 public static class CommandHandler{
 
-    //Handles Extraction of commands and parameters given a message sent by server or client
-    
-    //Could use a parser
+    public static String getCommand(String msg){
+        
+        return msg.split(" ")[0];
 
+    }
+    
+    public static String[] getParameters(String command, String msg){
+        
+        msg = msg.replace(command + " ", "");
+        return msg.split(" ");
+        
+    }
+    
 }
