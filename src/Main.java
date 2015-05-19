0a6ec72e870aadfb6ad3c90fe151b137067e4fd8
Genesis Palaganas <itetsky123@gmail.com>
2015-05-19 11:44:38 +0800

    Changed Game name to Magus Arena

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main{
     public static void main(String args[]){
         
         try{
-            Game game = new Game("Game","localhost");
+            Game game = new Game("Magus Arena","localhost");
             GameContainer container = new GameContainer(game);
 
             container.start();
